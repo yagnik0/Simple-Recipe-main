44b90acf608ac710bfdf807476e3ee95e0c59ad3
yagnik0 <yashyagnik456@.com>
2024-01-30 15:07:36 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+      young: ["Young Serif","serif"]
+    }
+  },
+  plugins: [],
+}
+
